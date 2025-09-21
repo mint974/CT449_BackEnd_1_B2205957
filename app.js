99960b35d3da6272973d6d7b5d2f6160ec761be9
mint974 <mtan090704@gmail.com>
2025-09-21 14:52:33 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
+const contactRoutes = require("./app/routes/contact.route");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactRoutes);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to Contact Book application' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Contact Book application" });
 });
 
 module.exports = app;
